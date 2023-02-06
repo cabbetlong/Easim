c17dc9ce7570c7f3d28f9c27ddf704aebee0a068
龙柏安 <longba@dustess.com>
2023-02-06 15:05:03 +0800

    chore: update

--- lua/easim/plugins/colorscheme.lua
+++ lua/easim/plugins/colorscheme.lua
@@ -2,15 +2,6 @@ local M = {
   "echasnovski/mini.base16",
   lazy = false,
   priority = 999,
-  opts = {
-    colorscheme = "gruvbox",
-    background = "dark",
-  },
 }
-
-function M.config(_, opts)
-  vim.opt.background = opts.background
-  vim.cmd("colorscheme " .. opts.colorscheme)
-end
 
 return M
