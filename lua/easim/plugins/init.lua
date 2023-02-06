3a8e61db8e424cc681e2572ab6b13baf2ef3d363
cabbet <cabbetlong@gmail.com>
2023-02-06 15:43:42 +0800

    fix: change LazyVim to Easim

--- lua/easim/plugins/init.lua
+++ lua/easim/plugins/init.lua
@@ -2,5 +2,5 @@ require("easim.core").init()
 
 return {
   { "folke/lazy.nvim" },
-  { "LazyVim/LazyVim", priority = 10000, lazy = false, config = true },
+  { "cabbetlong/Easim", priority = 10000, lazy = false, config = true },
 }
