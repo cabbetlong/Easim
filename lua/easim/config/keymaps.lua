2cb2b361c298e7d1ebca4844ec1c3f2b4152e118
cabbet <cabbetlong@gmail.com>
2023-02-08 17:08:01 +0800

    feat: update

--- lua/easim/config/keymaps.lua
+++ lua/easim/config/keymaps.lua
@@ -1,10 +1,32 @@
+local opts = require("easim.core").keys
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+local function cmd(c)
+  return "<cmd>" .. c .. "<cr>"
+end
+
 return function()
-  local keys = require("easim.core").keys
-  if not keys.all then
+  if not opts.all then
     return
   end
 
-  if keys.basic then
-    vim.keymap.set("n", "<leader>q", "<cmd>qa<cr>")
+  if opts.basic then
+    -- Space as leader
+    map({ "n", "v" }, "<space>", "<nop>", { desc = "Unset space" })
+
+    map("n", "<leader>q", cmd("q"), { desc = "Quit" })
+    map("n", "<leader>Q", cmd("q!"), { desc = "Quit forcely" })
+    map("n", "<leader>w", cmd("w"), { desc = "Write buffer" })
+
+    -- search
+    map("n", "<esc>", cmd("noh"), { desc = "No hilight" })
   end
 end
