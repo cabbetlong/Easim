09ca0d033aebe034e03010abc91de1adc8751df7
cabbet <cabbetlong@gmail.com>
2023-02-17 14:29:06 +0800

    feat: update

--- lua/easim/plugins/util.lua
+++ lua/easim/plugins/util.lua
@@ -2,30 +2,6 @@ return {
   "nvim-lua/plenary.nvim",
 
   {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-      plugins = { spelling = true },
-    },
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-      local keymaps = {
-        mode = { "n", "v" },
-        ["g"] = { name = "+goto&lsp" },
-        ["ys"] = { name = "+surround" },
-        ["]"] = { name = "+next" },
-        ["["] = { name = "+prev" },
-        ["<leader><tab>"] = { name = "+tabs" },
-        ["<leader>f"] = { name = "+file/find" },
-        ["<leader>q"] = { name = "+quit/session" },
-        ["<leader>t"] = { name = "+toggle" },
-      }
-      wk.register(keymaps)
-    end,
-  },
-
-  {
     "dstein64/vim-startuptime",
     cmd = "StartupTime",
     config = function()
